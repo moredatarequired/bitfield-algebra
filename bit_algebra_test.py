1df0c7c381e7ee9894ff3533c0d26f04f1221708
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-16 15:14:25 -0500

    Make common test declarations

--- bit_algebra_test.py
+++ bit_algebra_test.py
@@ -1,12 +1,17 @@
 from bit_algebra import Bit
 
 
+t = Bit(1)
+f = Bit(0)
+x, y, z, w = [Bit(v) for v in "xyzw"]
+
+
 def test_bits_one_equals_one():
-    assert Bit(1) == Bit(1)
+    assert t == Bit(1)
 
 
 def test_bits_zero_equals_zero():
-    assert Bit(0) == Bit(0)
+    assert f == Bit(0)
 
 
 def test_bits_zero_ne_one():
@@ -15,15 +20,14 @@ def test_bits_zero_ne_one():
 
 
 def test_same_names_equal():
-    assert Bit("x") == Bit("x")
+    assert x == Bit("x")
 
 
 def test_different_names_not_equal():
-    assert Bit("x") != Bit("y")
+    assert x != y
 
 
 def test_not_inverts_bits():
-    t, f = Bit(1), Bit(0)
     assert t == ~f
     assert ~t == f
     assert t == ~~t
@@ -32,7 +36,6 @@ def test_not_inverts_bits():
 
 
 def test_and1_simplifies():
-    t, f, x = Bit(1), Bit(0), Bit("x")
     assert t & f == f
     assert f & t == f
     assert t & t == t
@@ -41,20 +44,17 @@ def test_and1_simplifies():
 
 
 def test_and0_simplifies():
-    f, x = Bit(0), Bit("x")
     assert f & f == f
     assert f & x == f
     assert x & f == f
 
 
 def test_and_symbolic():
-    x, y = Bit("x"), Bit("y")
     assert str(x & y) == "x&y"
     assert x & y == y & x
 
 
 def test_and_symbolic_simplifies():
-    x, y = Bit("x"), Bit("y")
     assert x & x == x
     assert (x & y) & (y & x) == x & y
     assert str(x & x) == "x"
@@ -62,7 +62,6 @@ def test_and_symbolic_simplifies():
 
 
 def test_and_not_simplifies():
-    x, y, f = Bit("x"), Bit("y"), Bit(0)
     assert x & ~x == f
     assert ~x & x == f
     assert ~(x & y) & (y & x) == f
@@ -70,7 +69,6 @@ def test_and_not_simplifies():
 
 
 def test_xor_zero_simplifies():
-    t, f, x = Bit(1), Bit(0), Bit("x")
     assert t ^ f == t
     assert f ^ t == t
     assert f ^ f == f
@@ -79,7 +77,10 @@ def test_xor_zero_simplifies():
 
 
 def test_xor_one_inverts():
-    t, f, x = Bit(1), Bit(0), Bit("x")
     assert t ^ t == f
     assert x ^ t == ~x
     assert t ^ x == ~x
+
+
+def test_xor_symbolic_simplifies():
+    pass
