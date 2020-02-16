a138b4f084a31ef52c2562fc955230328503bb00
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-02-16 13:22:00 -0500

    Clean up implementation of Not

--- bit_algebra.py
+++ bit_algebra.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class Bit:
     def __init__(self, value):
+        """Value should be 0, 1, or a unique name."""
         self.value = value
 
     def __repr__(self):
@@ -22,25 +23,22 @@ class Bit:
         return Not(Bit(self.value[:]))
 
 
-# A bit can be True, or False, or variable / unknown.
-class Variable:
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return f"Variable({self.name})"
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __not__(self):
-        return Not(self)
-
-
 class Not:
+    """Unitary NOT, represented with `~` rather than `!` or `not`."""
+
     def __init__(self, child):
         self.child = child
 
+    def __repr__(self):
+        return f"Not({repr(self.child)})"
+
+    def __str__(self):
+        return f"~{str(self.child)}"
+
+    def __invert__(self):
+        return self.child
+
+    @property
+    def value(self):
+        if self.child.value in (0, 1):
+            return not self.child.value
